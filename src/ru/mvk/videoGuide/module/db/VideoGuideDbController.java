eff417a04bae5fd5f26fa0751d026fc9ada996bb
raptor-mvk <m.v.kamorin@gmail.com>
2015-03-19 21:51:36 +0300

    Fixed database update for 'ё' problem

--- src/ru/mvk/videoGuide/module/db/VideoGuideDbController.java
+++ src/ru/mvk/videoGuide/module/db/VideoGuideDbController.java
@@ -18,19 +18,13 @@ public class VideoGuideDbController extends SQLiteAbstractDbController {
   @Override
   protected boolean updateDbSchema(int fromDbVersion) {
     if (fromDbVersion < 2) {
-      execute("alter table film add column lowerName text");
-      List<?> rows = executeQuery("select rowid, name from film;");
-      for (Object row : rows) {
-        @NotNull String id = ((Object[]) row)[0].toString();
-        @NotNull String name = ((Object[]) row)[1].toString().toLowerCase();
-        @NotNull String escapedName = name.replaceAll("'", "''");
-        @NotNull String sqlQuery = "update film set lowerName='" + escapedName +
-            "' where rowid=" + id + ';';
-        execute(sqlQuery);
-      }
+      updateFromVersion1();
     }
     if (fromDbVersion < 3) {
       execute("create table disc (number int, size int);");
+    }
+    if (fromDbVersion < 4) {
+      updateFromVersion3();
     }
     return true;
   }
@@ -50,6 +44,31 @@ public class VideoGuideDbController extends SQLiteAbstractDbController {
 
   @Override
   protected int getAppDbVersion() {
-    return 3;
+    return 4;
+  }
+
+  private void updateFromVersion1() {
+    execute("alter table film add column lowerName text");
+    List<?> rows = executeQuery("select rowid, name from film;");
+    for (Object row : rows) {
+      @NotNull String id = ((Object[]) row)[0].toString();
+      @NotNull String name = ((Object[]) row)[1].toString().toLowerCase();
+      @NotNull String escapedName = name.replace("'", "''");
+      @NotNull String sqlQuery = "update film set lowerName='" + escapedName +
+          "' where rowid=" + id + ';';
+      execute(sqlQuery);
+    }
+  }
+
+  private void updateFromVersion3() {
+    List<?> rows = executeQuery("select rowid, lowerName from film;");
+    for (Object row : rows) {
+      @NotNull String id = ((Object[]) row)[0].toString();
+      @NotNull String lowerName = ((Object[]) row)[1].toString().toLowerCase();
+      @NotNull String fixedLowerName = lowerName.replace('ё', 'е');
+      @NotNull String sqlQuery = "update film set lowerName='" + fixedLowerName +
+          "' where rowid=" + id + ';';
+      execute(sqlQuery);
+    }
   }
 }
